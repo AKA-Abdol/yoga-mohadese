9925598760c1d2549816f84523accb5fca2be2ae
Amirhossein <amirabdolii@gmail.com>
2023-07-08 12:58:26 +0330

    feat: tailwind config setup

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,16 +1,14 @@
-import { FC } from "react"
+import { FC } from "react";
 
 const Auth: FC = () => {
-    return (
-        <div className={"w-full h-full flex flex-col justify-between"}>
-            <div className={"_form_ w-full px-4 border border-black"}>
-                inputs
-            </div>
-            <div className="_image_ w-full border border-black">
-                image here
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={"w-full h-full flex flex-col justify-between"}>
+      <div className={"_form_ w-full px-4 border border-black py-md"}>
+        inputs
+      </div>
+      <div className="_image_ w-full border border-black">image here</div>
+    </div>
+  );
+};
 
 export default Auth;
