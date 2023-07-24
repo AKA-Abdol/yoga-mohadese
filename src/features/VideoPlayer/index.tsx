ce420636fd78fa0e44e0b7155b8cb9687d2ec24f
Amirhossein <amirabdolii@gmail.com>
2023-07-24 21:34:47 +0330

    feat(user): video player initialized

--- src/features/VideoPlayer/index.tsx
+++ src/features/VideoPlayer/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
 const VideoPlayer: FC = () => {
-    return (
-        <div>
-            some videos
-        </div>
-    )
-}
+  return (
+    <div className={`w-full h-full`}>
+      <video className={`w-full h-full object-contain`} controls controlsList="nodownload">
+        <source src={process.env.REACT_APP_TEST_VIDEO} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
 
 export default VideoPlayer;
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 import VideoPlayer from "src/features/VideoPlayer";
 
 const User: FC = () => {
-    return (
-        <div className="w-full h-full">
-            <VideoPlayer />
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full lg:w-1/2 bg-black">
+        <VideoPlayer />
+      </div>
+    </div>
+  );
+};
 
 export default User;
