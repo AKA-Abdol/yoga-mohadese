b2084a75f1c06886af2a20b3ed17ee06d2cf69dc
Amirhossein <amirabdolii@gmail.com>
2023-07-25 16:20:51 +0330

    fix(user): subtitle for video changed

--- src/features/VideoPlayer/components/SessionList/SessionItem.tsx
+++ src/features/VideoPlayer/components/SessionList/SessionItem.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 
 const SessionItem: FC = () => {
-    return (
-        <div className="h-full avatar ml-sm flex flex-col items-center space-y-sm cursor-pointer">
-            <p className={`text-lg text-primary-light`}>ویدیو شماره ۲</p>
-            <p className="text-xs text-primary-light">توضیحات ویدیو ...</p>
-            <div className="h-40 mask mask-squircle">
-                <img src={process.env.REACT_APP_TEST_PIC} alt="mohadese"/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="avatar ml-sm flex flex-col items-center space-y-sm cursor-pointer">
+      <p className={`text-lg text-primary-light`}>ویدیو شماره ۲</p>
+      <div className="w-40 mask mask-squircle">
+        <img src={process.env.REACT_APP_TEST_PIC} alt="mohadese" />
+      </div>
+      <p className="text-xs text-primary-light text-center">
+        یک سری درباره توضیحات که بیرون میزنه!
+      </p>
+    </div>
+  );
 };
 
 export default SessionItem;
