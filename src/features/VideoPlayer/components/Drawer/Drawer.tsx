ced622505cf9fdf22057e14995d1e9cea0824874
aka-abdol <thegrolam@gmail.com>
2023-07-29 00:12:23 +0330

    fix(user): animation smotheness done

--- src/features/VideoPlayer/components/Drawer/Drawer.tsx
+++ src/features/VideoPlayer/components/Drawer/Drawer.tsx
@@ -8,9 +8,9 @@ const Drawer: FC<DrawerProps> = (props) => {
       className={classNames(
         "fixed top-0 left-0 right-0 mx-auto z-10",
         "w-full lg:w-1/2 h-full",
-        "transition-all visible opacity-100 duration-500 ease-in-out",
+        "transition-all duration-500 ease-in-out",
         "bg-primary-light text-primary-dark",
-        !props.show && "invisible opacity-0"
+        props.show ? "visible opacity-100" : "invisible opacity-0"
       )}
     >
       {props.children && props.children}
