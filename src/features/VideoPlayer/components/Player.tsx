0a4f3131543257b65bd2d2854ccce5bcf1590d6d
aka-abdol <thegrolam@gmail.com>
2023-08-27 19:56:56 +0330

    feat(player): idea for counting worked!

--- src/features/VideoPlayer/components/Player.tsx
+++ src/features/VideoPlayer/components/Player.tsx
@@ -1,4 +1,11 @@
-import { FC, useContext } from "react";
+import {
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { VideoContext } from "../VideoContext";
 import { useQuery } from "@tanstack/react-query";
 import api from "src/services";
@@ -6,8 +13,12 @@ import { BASE_TERM_URL } from "src/pages/Admin/Terms/api.data";
 import { WithVideos } from "src/pages/Admin/Terms/[id]/videos/types";
 import { ITermApi } from "../types";
 import Loading from "src/components/ui/Loading";
+import MOCK_VIDEO from "src/assets/videos/mock-video.mp4";
 
 const Player: FC = (props) => {
+  const [videoState, setVideoState] = useState<"play" | "pause">("pause");
+  const [seenTime, setSeenTime] = useState(0);
+  const videoRef = useRef<HTMLVideoElement>(null);
   const videoContext = useContext(VideoContext);
   const term = useQuery({
     queryKey: [
@@ -19,6 +30,46 @@ const Player: FC = (props) => {
       `${BASE_TERM_URL}/${videoContext.selected.termId}`
     ),
   });
+
+  const CountUpSeenTime = () => {
+    setSeenTime((prevTime) => prevTime + 1);
+  };
+
+  console.log("now: ", seenTime);
+
+  useEffect(() => {
+    if (videoState === "pause") return;
+
+    const interval = setInterval(CountUpSeenTime, 1000);
+    return () => clearInterval(interval);
+  }, [videoState]);
+
+  useEffect(() => {
+    videoRef.current?.pause();
+  }, [videoRef]);
+
+  const toggleVideoState = useCallback(() => {
+    if (!videoRef.current) return;
+    if (videoState === "pause") {
+      console.log("playing now: ", videoRef.current);
+      setVideoState("play");
+      videoRef.current?.play();
+    } else {
+      console.log("Pausing now: ", videoRef.current);
+
+      setVideoState("pause");
+      videoRef.current?.pause();
+    }
+  }, [videoState]);
+
+  useEffect(() => {
+    const handleSpacePress = (e: KeyboardEvent) => {
+      if (e.key === " ") toggleVideoState();
+    };
+    window.addEventListener("keypress", handleSpacePress);
+
+    return () => window.removeEventListener("keypress", handleSpacePress);
+  }, [toggleVideoState]);
 
   if (!videoContext.selected.termId)
     return (
@@ -43,13 +94,20 @@ const Player: FC = (props) => {
 
   return (
     <video
+      ref={videoRef}
       className={`w-full h-full object-contain`}
       controls
       controlsList="nodownload"
+      poster={selectedSessionVideo[0].thumbnail}
+      onClick={(e) => {
+        e.preventDefault();
+        toggleVideoState();
+      }}
     >
       <source
         src={
-          selectedSessionVideo.length ? selectedSessionVideo[0].link : undefined
+          // selectedSessionVideo.length ? selectedSessionVideo[0].link : undefined
+          MOCK_VIDEO
         }
         type="video/mp4"
       />
