20c17a85fe1e229de290ace2e11453d7310cd956
aka-abdol <thegrolam@gmail.com>
2023-12-06 10:38:12 +0330

    fix: course shop removed

--- back/src/modules/course/course.shop.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { CourseService } from "./course.service";
-
-@Injectable()
-export class ShopService {
-    constructor(private readonly courseService: CourseService) {}
-
-}
